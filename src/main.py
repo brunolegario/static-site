2f48b0342c4a6436329157796a2d4733535c85b9
brunolegario <brunolegario@gmail.com>
2025-11-29 17:11:20 -0300

    finish recursive functions

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from textnode import TextNode, TextType
 from inline_markdown import text_to_textnodes
-from blocktype import markdown_to_html_node
+from blocktype import markdown_to_html_node, extract_title
 
 def copy_static_to_public():
   src_dir = os.path.join(os.path.dirname(__file__), '..', 'static')
@@ -29,7 +29,51 @@ def copy_files(src, dest):
       shutil.copy2(src_path, dest_path)
       print(f"Copied: {src_path} -> {dest_path}")
 
+def generate_page(from_path, template_path, dest_path):
+  print(f"Generating page from {from_path} to {dest_path} using template {template_path}")
+
+  with open(from_path, 'r') as f:
+    content = f.read()
+
+  with open(template_path, 'r') as f:
+    template = f.read()
+
+  html_content = markdown_to_html_node(content).to_html()
+  title = extract_title(content)
+
+  template = template.replace("{{ Title }}", title)
+  template = template.replace("{{ Content }}", html_content)
+
+  dest_dir = os.path.dirname(dest_path)
+  if not os.path.exists(dest_dir):
+    os.makedirs(dest_dir)
+
+  with open(dest_path, 'w') as f:
+    f.write(template)
+
+def generate_pages(content_path, template_path, destination_path):
+  for item in os.listdir(content_path):
+    src_path = os.path.join(content_path, item)
+    
+    if os.path.isdir(src_path):
+      dest_path = os.path.join(destination_path, item)
+      generate_pages(src_path, template_path, dest_path)
+
+    elif item.endswith('.md'):
+      html_filename = item.replace('.md', '.html')
+      dest_path = os.path.join(destination_path, html_filename)
+      generate_page(src_path, template_path, dest_path)
+
+
+
 def main():
   copy_static_to_public()
+  
+  script_dir = os.path.dirname(__file__)
+  content_dir = os.path.join(script_dir, '..', 'content')
+  template_path = os.path.join(script_dir, '..', 'template.html')
+  public_dir = os.path.join(script_dir, '..', 'public')
+  
+  generate_pages(content_dir, template_path, public_dir)
 
 main()
