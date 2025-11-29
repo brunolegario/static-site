7ae089c43e5aef2d0eaa6d47d0ec166bd0e6c47a
brunolegario <brunolegario@gmail.com>
2025-11-29 00:08:58 -0300

    copying files between folders

--- src/main.py
+++ src/main.py
@@ -1,31 +1,35 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 from inline_markdown import text_to_textnodes
 from blocktype import markdown_to_html_node
 
+def copy_static_to_public():
+  src_dir = os.path.join(os.path.dirname(__file__), '..', 'static')
+  dest_dir = os.path.join(os.path.dirname(__file__), '..','public')
+
+  if os.path.exists(dest_dir):
+    shutil.rmtree(dest_dir)
+  
+  os.mkdir(dest_dir)
+  
+  copy_files(src_dir, dest_dir)
+
+def copy_files(src, dest):
+  if not os.path.exists(dest):
+    os.mkdir(dest)
+  
+  for item in os.listdir(src):
+    src_path = os.path.join(src, item)
+    dest_path = os.path.join(dest, item)
+    
+    if os.path.isdir(src_path):
+      copy_files(src_path, dest_path)
+    else:
+      shutil.copy2(src_path, dest_path)
+      print(f"Copied: {src_path} -> {dest_path}")
+
 def main():
-  # node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-  # print(node)
-
-  md = """
-This is **bolded** paragraph
-text in a p
-tag here
-
-> This is another paragraph with _italic_ text and `code` here
-
-1. First item
-2. Second item
-
-```
-def example():
-    print("Hello, World!")
-```
-
-- Unordered item one
-- Unordered item two
-"""
-
-  html_node = markdown_to_html_node(md)
-  print(html_node.to_html())
+  copy_static_to_public()
 
 main()
