44b1b9f64190cb9e9c37fd8e2135dbf862931aea
brunolegario <brunolegario@gmail.com>
2025-11-29 17:33:00 -0300

    preparing for github pages

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import shutil
 from textnode import TextNode, TextType
 from inline_markdown import text_to_textnodes
@@ -6,7 +7,7 @@ from blocktype import markdown_to_html_node, extract_title
 
 def copy_static_to_public():
   src_dir = os.path.join(os.path.dirname(__file__), '..', 'static')
-  dest_dir = os.path.join(os.path.dirname(__file__), '..','public')
+  dest_dir = os.path.join(os.path.dirname(__file__), '..','docs')
 
   if os.path.exists(dest_dir):
     shutil.rmtree(dest_dir)
@@ -29,7 +30,7 @@ def copy_files(src, dest):
       shutil.copy2(src_path, dest_path)
       print(f"Copied: {src_path} -> {dest_path}")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
   print(f"Generating page from {from_path} to {dest_path} using template {template_path}")
 
   with open(from_path, 'r') as f:
@@ -43,6 +44,8 @@ def generate_page(from_path, template_path, dest_path):
 
   template = template.replace("{{ Title }}", title)
   template = template.replace("{{ Content }}", html_content)
+  template = template.replace("href=/", f"href={basepath}")
+  template = template.replace("src=/", f"src={basepath}")
 
   dest_dir = os.path.dirname(dest_path)
   if not os.path.exists(dest_dir):
@@ -51,29 +54,34 @@ def generate_page(from_path, template_path, dest_path):
   with open(dest_path, 'w') as f:
     f.write(template)
 
-def generate_pages(content_path, template_path, destination_path):
+def generate_pages(content_path, template_path, destination_path, basepath):
   for item in os.listdir(content_path):
     src_path = os.path.join(content_path, item)
     
     if os.path.isdir(src_path):
       dest_path = os.path.join(destination_path, item)
-      generate_pages(src_path, template_path, dest_path)
+      generate_pages(src_path, template_path, dest_path, basepath)
 
     elif item.endswith('.md'):
       html_filename = item.replace('.md', '.html')
       dest_path = os.path.join(destination_path, html_filename)
-      generate_page(src_path, template_path, dest_path)
+      generate_page(src_path, template_path, dest_path, basepath)
 
 
 
 def main():
+  args = sys.argv
+  
+  basepath = "/"
+  if args[0]:
+    basepath = args[0]
+
   copy_static_to_public()
   
   script_dir = os.path.dirname(__file__)
   content_dir = os.path.join(script_dir, '..', 'content')
   template_path = os.path.join(script_dir, '..', 'template.html')
-  public_dir = os.path.join(script_dir, '..', 'public')
+  public_dir = os.path.join(script_dir, '..', 'docs')
   
-  generate_pages(content_dir, template_path, public_dir)
-
+  generate_pages(content_dir, template_path, public_dir, basepath)
 main()
